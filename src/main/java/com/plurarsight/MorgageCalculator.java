fbe8770c5e227a43e08963c2dd7ac89d3e657303
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-13 15:35:33 -0400

    Updaded Java Files

--- src/main/java/com/plurarsight/MorgageCalculator.java
+++ src/main/java/com/plurarsight/MorgageCalculator.java
@@ -1,8 +1,28 @@
 package com.plurarsight;
-
+import java.util.Scanner;
 public class MorgageCalculator {
     public static void main(String[] args) {
+
+        Scanner = new Scanner(System.in);
+
+        System.out.println("Hi, what is your principal?");
+        double Principal = scanner.next.Double();
+
+        System.out.println("what is your annual interest rate?" );
+        float interestRate = scanner.nextFloat();
+
+        System.out.println("What is your loan term in years?");
         
+
+
+
+
+
+
+
+
+
+
 
     }
 }
