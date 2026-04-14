4a1a98fbc10fd8442459f6e467502ea28f246be0
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-13 22:00:26 -0400

    Created formulas that will calculte the interest rate earned by the custumer

--- src/main/java/com/plurarsight/FutureValue.java
+++ src/main/java/com/plurarsight/FutureValue.java
@@ -15,10 +15,27 @@ public class FutureValue {
 
          System.out.print("Enter the number of years: ");
          int years = scanner.nextInt();
-         
+
+         //Now, converting percentages to decimal
+         double rateDecimal = annualRate / 100;
+
+         //Setting up the daily compounding
+         int daysPerYear = 365;
+         int totalDays = daysPerYear * years;
+
+         // Setting the future Value formula
+         double futureValue = principal *
+                 Math.pow(1 + (rateDecimal / daysPerYear), totalDays);
+
+         //Now, calculating the total interest earned by the custumer
+         double totalInterest = futureValue - principal;
 
 
 
 
-    }
+
+
+
+
+     }
 }
