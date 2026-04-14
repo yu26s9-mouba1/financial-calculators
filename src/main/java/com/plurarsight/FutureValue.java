fc49d8cd6a44921934b72e7d173d53f103fb18aa
Moubarak Congacou <cmouba1999@gmail.com>
2026-04-13 21:54:49 -0400

    Created new calculator (Future value calculator)

---
+++ src/main/java/com/plurarsight/FutureValue.java
@@ -0,0 +1,24 @@
+package com.plurarsight;
+import java.util.Scanner;
+
+
+public class FutureValue {
+     public static void main(String[] args) {
+         Scanner scanner = new Scanner(System.in);
+
+         //Prompting the user for inputs
+         System.out.print("Enter the deposit amount: ");
+         double principal = scanner.nextDouble();
+
+         System.out.print("Enter the annual interest rate (e.g. 1.75): ");
+         double annualRate = scanner.nextDouble();
+
+         System.out.print("Enter the number of years: ");
+         int years = scanner.nextInt();
+         
+
+
+
+
+    }
+}
